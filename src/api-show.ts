92d97d1a7b362e1b4f1bd17637c1e4574faf137a
goenning <me@goenning.net>
2017-05-12 07:29:46 +0100

    allow now as parameter

--- src/api-show.ts
+++ src/api-show.ts
@@ -2,43 +2,51 @@ import * as AWS from 'aws-sdk';
 import config from './config';
 
 import { toTicks, formatResult, applyInterval } from './utils';
+import { pull } from './pull';
 import { PullResult } from './interfaces';
 
 const client = new AWS.DynamoDB.DocumentClient();
 
-export const handler = async (event: any, context: any, callback: any) => {
-    try {
-        const body = Object.assign({}, event.queryStringParameters, event.pathParameters);
-        console.log(`Input:`, body);
-
-        const ticks = toTicks(body.date);
-
+const getByTicks = (ticks: number): Promise<PullResult> => {
+    return new Promise((resolve, reject) => {
         client.get({
             TableName: config.dbTable,
             Key: {
                 ticks
             }
         }, (err, data) => {
-
             console.log(`DynamoDB GET:`, err, data);
             if (err) {
-                return callback(err, null);
+                return reject(err);
             }
-
-            const result = data.Item as PullResult;
-
-            if (result) {
-                return callback(null, {
-                    statusCode: 200,
-                    body: JSON.stringify(formatResult(result, body.format))
-                });
-            } else {
-                return callback(null, {
-                    statusCode: 404
-                });
-            }
-
+            return resolve(data.Item);
         });
+    });
+};
+
+export const handler = async (event: any, context: any, callback: any) => {
+    try {
+        const body = Object.assign({}, event.queryStringParameters, event.pathParameters);
+        console.log(`Input:`, body);
+
+        let result: PullResult;
+        if (body.date === 'now') {
+            result = await pull();
+        } else {
+            const ticks = toTicks(body.date);
+            result = await getByTicks(ticks);
+        }
+
+        if (result) {
+            return callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(formatResult(result, body.format))
+            });
+        } else {
+            return callback(null, {
+                statusCode: 404
+            });
+        }
     } catch (err) {
         return callback(null, {
             statusCode: 400,
